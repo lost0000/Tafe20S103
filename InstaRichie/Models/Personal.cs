2ed3009285a9b61cad8471ffe8615376fd5cacc6
virroshan <virroshan08@gmail.com>
2020-04-13 15:01:01 +0930

    modified with Update, Delete and Add

---
+++ InstaRichie/Models/Personal.cs
@@ -0,0 +1,30 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class Personal
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DOB { get; set; }
+
+        public string Gender { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string MobilePhone { get; set; }
+     
+
+
+    }
+}
